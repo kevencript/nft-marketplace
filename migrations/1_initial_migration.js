bd5ce4db1f5c426e9c211568259577e207fb77ab
Gabriel Braga <kevencript@gmail.com>
2021-11-20 11:55:13 -0300

    feat: truffle migration to deploy the contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Contracts = artifacts.require("Brazukas");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Contracts);
 };
