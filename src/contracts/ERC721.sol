0842e1e2806a4a15c8e9c3f2f7670725cb5d1ce5
Gabriel Braga <kevencript@gmail.com>
2021-11-21 19:07:34 -0300

    feat: created mint function on ERC721 contract

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -8,5 +8,18 @@ contract ERC721 {
     // owner to how many tokens owned
     mapping(address => uint256) private _ownedTokensCount;
 
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        address owner = _tokenOwner[tokenId];
+        return owner != address(0);
+    }
+
+    function _mint(address receiver, uint256 tokenId) internal {
+        require(receiver != address(0), "ERC721: minting the zero address");
+        require(!_exists(tokenId), "ERC721: token already minted");
+
+        _tokenOwner[tokenId] = receiver;
+        _ownedTokensCount[receiver] += 1;
+    }
+
     constructor() {}
 }
