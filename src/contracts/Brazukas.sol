476234d22489567bc52fcf2fe662860db09e5d88
Gabriel Braga <kevencript@gmail.com>
2021-11-21 19:53:26 -0300

    fix: fixing text on Brazukas requirement messages for minting

--- src/contracts/Brazukas.sol
+++ src/contracts/Brazukas.sol
@@ -4,5 +4,20 @@ pragma solidity ^0.8.9;
 import "./ERC721Connector.sol";
 
 contract Brazukas is ERC721Connector {
+    string[] public BrazukasArray;
+
+    mapping(string => bool) _brazukaExists;
+
+    function mint(string memory _brazuka) public {
+        require(!_brazukaExists[_brazuka], "Error - Brazuka already exists");
+
+        BrazukasArray.push(_brazuka);
+        uint256 _id = BrazukasArray.length - 1;
+
+        _mint(msg.sender, _id);
+
+        _brazukaExists[_brazuka] = true;
+    }
+
     constructor() ERC721Connector("Brazukas", "BZKS") {}
 }
