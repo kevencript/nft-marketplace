bd0805a4b39125923847f3722efd9954363c37f5
Gabriel Braga <kevencript@gmail.com>
2021-11-21 19:17:49 -0300

    feat: connecting ERC721 contract on connector

--- src/contracts/ERC721Connector.sol
+++ src/contracts/ERC721Connector.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./ERC721Metadata.sol";
+import "./ERC721.sol";
 
-contract ERC721Connector is ERC721Metadata {
+contract ERC721Connector is ERC721Metadata, ERC721 {
     // Construct ERC721Metadata for our main contract
     constructor(string memory name, string memory symbol)
         ERC721Metadata(name, symbol)
