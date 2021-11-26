6e31d4359758a87234bf48aadcb654f066d94939
Gabriel Braga <kevencript@gmail.com>
2021-11-26 17:14:05 -0300

    feat: creating Enumerable contract

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -19,7 +19,7 @@ contract ERC721 {
         return owner != address(0);
     }
 
-    function _mint(address receiver, uint256 tokenId) internal {
+    function _mint(address receiver, uint256 tokenId) internal virtual {
         require(receiver != address(0), "ERC721: minting the zero address");
         require(!_exists(tokenId), "ERC721: token already minted");
 
--- src/contracts/ERC721Connector.sol
+++ src/contracts/ERC721Connector.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./ERC721Metadata.sol";
-import "./ERC721.sol";
+import "./ERC721Enumerable.sol";
 
-contract ERC721Connector is ERC721Metadata, ERC721 {
+contract ERC721Connector is ERC721Metadata, ERC721Enumerable {
     // Construct ERC721Metadata for our main contract
     constructor(string memory name, string memory symbol)
         ERC721Metadata(name, symbol)
