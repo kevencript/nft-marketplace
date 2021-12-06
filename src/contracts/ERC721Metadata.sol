1d4bcceca02f450671ca148fdaf5b0543ff86f6f
Gabriel Braga <kevencript@gmail.com>
2021-12-06 20:49:19 -0300

    feat: registring interface for ERC721Metadata

--- src/contracts/ERC721Metadata.sol
+++ src/contracts/ERC721Metadata.sol
@@ -2,12 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "./interfaces/IERC721Metadata.sol";
+import "./ERC165.sol";
 
-contract ERC721Metadata is IERC721Metadata {
+contract ERC721Metadata is IERC721Metadata, ERC165 {
     string private _name;
     string private _symbol;
 
     constructor(string memory named, string memory symbolified) {
+        bytes4 interfaceIds = bytes4(
+            keccak256("name(bytes4)") ^ keccak256("symbol(bytes4)")
+        );
+        _registerInterface(interfaceIds);
+
         _name = named;
         _symbol = symbolified;
     }
